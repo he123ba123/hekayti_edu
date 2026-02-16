b74504ecfcfb6dc42c8b19bb37188af6f5aa5240
heba <hebatarekg@gamil.com>
2026-02-16 12:38:06 +0200

    fix conflit marge

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -2,7 +2,5 @@ class ApiConstants {
   static const baseUrl =
       'https://kids-learning-api-1073149557000.europe-west1.run.app/';
   static const storyEndPoint = 'story';
-}
-  static const baseUrl = 'https://kids-learning-api-1073149557000.europe-west1.run.app/';
-  static const quizEndPoint='quiz';
+  static const quizEndPoint = 'quiz';
 }
