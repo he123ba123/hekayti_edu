3cefcf107dcb8b6365b8630988725fe4bccfd736
marievasheaa <marievaheaa170@gmail>
2026-02-16 10:45:31 +0200

    add quiz end point

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,12 +1,4 @@
 class ApiConstants {
-  static const baseUrl = 'https://unappeased-krystyna-windowy.ngrok-free.dev';
-  static const login = '/api/login';
-  static const register = '/api/register';
-  static const verifyEmail = '/api/verify-otp';
-  static const otpPassword = '/api/verify-password';
-  static const resendOtp = '/api/resend-otp';
-  static const forgotPassword = '/api/forgot-password';
-  static const resetPassword = '/api/reset-password';
-  static const logout = '/api/logout';
-  static const sendFcmToken = '';
+  static const baseUrl = 'https://kids-learning-api-861940471411.europe-west1.run.app/';
+  static const quizEndPoint='quiz';
 }
